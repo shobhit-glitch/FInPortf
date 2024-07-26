db7aee5c91af91eba99491453cdca74a018b47fb
shobhit <shobhitsinghid@gmail.com>
2024-07-26 09:05:10 +0530

    first commit

--- src/Components/Exo.jsx
+++ src/Components/Exo.jsx
@@ -29,7 +29,7 @@ const Exo = () => {
                 <p className='mb-4 text-neutral-400 text-justify'>I developed a comprehensive college admission website using Node.js, Express, MongoDB, and Multer. The project features an intuitive form for candidates to submit personal data and pictures, with Multer ensuring secure and efficient image uploads. This project showcases my full-stack web development expertise, highlighting my ability to create dynamic, secure, and user-friendly applications using modern technologies like Mongoose and MongoDB.
 
 </p>
-                <span className='mr-2 mt-4 rounded bg-neutral-500 px-2 py-1 text-bold text-purple-800'>HTML</span>
+                <span className='mr-2 mt-4 rounded bg-neutral-500 px-2 py-1 text-sm text-purple-800'>HTML</span>
                 <span className='mr-2 mt-4 rounded bg-neutral-500 px-2 py-1 text-sm text-purple-800'>CSS</span>
                 <span className='mr-2 mt-4 rounded bg-neutral-500 px-2 py-1 text-sm text-purple-800'>JavaScript</span>
                 <span className='mr-2 mt-4 rounded bg-neutral-500 px-2 py-1 text-sm text-purple-800'>ExpressJs</span>
